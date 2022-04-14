5011bbb44d7e41b124da9af67bed8db229dc3dea
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-14 12:52:54 -0700

    navbar styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.scss';
-import Navbar from './components/navbar/Navbar';
 import Home from './views/home/Home';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Home />
     </div>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,28 @@
+import { Search, Notifications, ArrowDropDown } from '@mui/icons-material';
 import React from 'react';
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='container'>
-        <div className='left'></div>
-        <div className='right'></div>
+        <div className='left'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/en/6/6d/All_Elite_Wrestling.jpg'
+            alt='All Elite Wrestling logo'
+          />
+          <span>Featured</span>
+          <span>Pay-Per View</span>
+          <span>Dynamite</span>
+          <span>Rampage</span>
+          <span>Dark</span>
+          <span>Dark Elevation</span>
+        </div>
+        <div className='right'>
+          <Search />
+          <Notifications />
+          <ArrowDropDown />
+        </div>
       </div>
     </div>
   );

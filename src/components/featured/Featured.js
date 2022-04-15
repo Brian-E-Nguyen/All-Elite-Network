34562a7206e850074faca168f0a7d3298115c554
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-15 11:22:59 -0700

    init Featured

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,15 @@
+import './Featured.scss';
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <img
+        src='https://wrestlingnews.co/wp-content/uploads/2022/02/AEW-Dynamite-stage-arena.png'
+        className='jumbotron'
+        alt=''
+      />
+    </div>
+  );
+};
+
+export default Featured;

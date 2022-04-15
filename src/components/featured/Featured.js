52bd86734410ac1ff95321437cfbbdf120e7025c
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-15 11:34:22 -0700

    Featured: DoN image

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,13 +1,33 @@
+import { InfoOutlined, PlayArrow } from '@mui/icons-material';
 import './Featured.scss';
 
 const Featured = () => {
   return (
     <div className='featured'>
       <img
-        src='https://wrestlingnews.co/wp-content/uploads/2022/02/AEW-Dynamite-stage-arena.png'
+        src='https://staticg.sportskeeda.com/editor/2022/02/d50fa-16457460219022-1920.jpg'
         className='jumbotron'
         alt=''
       />
+      <div className='info'>
+        <img src='' alt='' />
+        <span className='description'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In porro
+          accusamus, totam et nulla numquam qui nisi, minus quod obcaecati
+          consequatur eaque nemo. Eos ducimus distinctio tempora id temporibus
+          et?
+        </span>
+        <div className='buttons'>
+          <button className='play'>
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className='more'>
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

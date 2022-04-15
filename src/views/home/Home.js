edffea0eafd7eac2e922cdabe34d5ec781bc7bd1
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-15 11:16:51 -0700

    Home: used another image

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='home'>
       <Navbar />
       <img
-        src='https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fwrestlingnews.co%2Fwp-content%2Fuploads%2F2022%2F02%2FAEW-Dynamite-stage-arena.png&sp=1649967497T81222fff90001de44768131bc4f8b9eb61d9da9bb289229b2af38d9940cfd5e9'
+        src='https://wrestlingnews.co/wp-content/uploads/2022/02/AEW-Dynamite-stage-arena.png'
         className='jumbotron'
         alt=''
       />

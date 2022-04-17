ec5b7658368bd0c634958fbf813b143060434e6b
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-17 14:50:27 -0700

    Home: import MediaList

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,11 +1,13 @@
 import './Home.scss';
 import Navbar from '../../components/navbar/Navbar';
 import Featured from '../../components/featured/Featured';
+import MediaList from '../../components/media/MediaList';
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
       <Featured />
+      <MediaList />
     </div>
   );
 };

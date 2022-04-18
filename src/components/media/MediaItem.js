4d4f1302aabc0af3757943ecaeec99e85c6fbb68
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-18 11:38:58 -0700

    MediaItem: image size and icons

--- src/components/media/MediaItem.js
+++ src/components/media/MediaItem.js
@@ -1,7 +1,29 @@
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from '@mui/icons-material';
 import './MediaItem.scss';
 
 const MediaItem = () => {
-  return <div className='media-item'>MediaItem</div>;
+  return (
+    <div className='media-item'>
+      {/* TODO: props for image */}
+      <img
+        src='https://staticg.sportskeeda.com/editor/2022/02/d50fa-16457460219022-1920.jpg'
+        alt=''
+      />
+      <div className='info'>
+        <div className='icons'>
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MediaItem;

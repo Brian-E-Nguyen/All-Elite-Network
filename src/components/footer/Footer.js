5b8fdc0944812d88234a37f149ab44e5bf481db2
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-18 13:56:16 -0700

    init Footer files

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,22 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='links'>
+        <span>Featured</span>
+        <span>Pay-Per View</span>
+        <span>Dynamite</span>
+        <span>Rampage</span>
+        <span>Dark</span>
+        <span>Dark Elevation</span>
+      </div>
+      <div className='disclaimer'>
+        This is a fan-made site. This is in no way affiliated with All Elite
+        Wrestling
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

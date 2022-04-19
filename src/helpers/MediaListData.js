cd31fcc341e72dc0f358e416fd4b8d66f208d47a
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-19 11:21:07 -0700

    init MediaListData.js

---
+++ src/helpers/MediaListData.js
@@ -0,0 +1,12 @@
+const payperview = [
+  {
+    title: 'Double or Nothing 2022',
+    runtime: '4 hours',
+    date: new Date('2022/5/29'),
+    description: 'temp',
+    imgSrc:
+      'https://staticg.sportskeeda.com/editor/2022/02/d50fa-16457460219022-1920.jpg',
+  },
+];
+
+export default payperview;

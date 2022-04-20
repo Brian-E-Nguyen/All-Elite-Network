72b6cb7ed91200fa1284a0f8ee3c1e13987b632d
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-20 09:16:55 -0700

    MediaItem: new font, font sizes, padding

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,12 +2,19 @@ import './Home.scss';
 import Navbar from '../../components/navbar/Navbar';
 import Featured from '../../components/featured/Featured';
 import MediaList from '../../components/media/MediaList';
+import Footer from '../../components/footer/Footer';
+import { payperview, dynamite } from '../../helpers/MediaListData';
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
       <Featured />
-      <MediaList />
+      <MediaList mediaListTitle={'Pay-Per View'} mediaListData={payperview} />
+      <MediaList mediaListTitle={'Dynamite'} mediaListData={dynamite} />
+      {/* <MediaList mediaListTitle={'Rampage'} />
+      <MediaList mediaListTitle={'Dark'} />
+      <MediaList mediaListTitle={'Dark Elevation'} /> */}
+      <Footer />
     </div>
   );
 };

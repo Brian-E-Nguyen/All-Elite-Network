02efd763d55513fc11a476dff510a997c8362beb
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-21 12:49:08 -0700

    MediaList: import MediaItemLarge

--- src/components/media/MediaList.js
+++ src/components/media/MediaList.js
@@ -1,6 +1,13 @@
 import './MediaList.scss';
+import MediaItemLarge from './MediaItemLarge';
 const MediaList = (props) => {
-  return <div className='media-list'>MediaList</div>;
+  return (
+    <div className='media-list'>
+      {props.mediaListData.map((data, i) => (
+        <MediaItemLarge key={i} data={data} />
+      ))}
+    </div>
+  );
 };
 
 export default MediaList;

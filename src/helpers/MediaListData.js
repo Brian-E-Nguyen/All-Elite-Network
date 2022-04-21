66ceda55712767e124148939f83b587e39be3f5d
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-21 10:30:51 -0700

    MediaListData: updated all links with global

--- src/helpers/MediaListData.js
+++ src/helpers/MediaListData.js
@@ -7,24 +7,21 @@ const payperview = [
     runtime: '4 hours',
     date: new Date(2022, 4, 29),
     description: 'temp',
-    imgSrc:
-      'https://staticg.sportskeeda.com/editor/2022/02/d50fa-16457460219022-1920.jpg',
+    imgSrc: `${CLOUDINARY_LINK}/Media/PPV/DoubleOrNothing2022.webp`,
   },
   {
     title: 'Revolution 2022',
     runtime: '4 hours',
     date: new Date(2022, 2, 26),
     description: 'temp',
-    imgSrc:
-      'https://www.cnet.com/a/img/hub/2022/03/06/26b2b84e-532d-4350-a4c2-a31ed0b5b267/crop-exact-aew-revolution-2022-twitter-1920x1080.jpg',
+    imgSrc: `${CLOUDINARY_LINK}/Media/PPV/Revolution2022.jpg`,
   },
   {
     title: 'Full Gear 2021',
     runtime: '4 hours',
     date: new Date(2021, 10, 13),
     description: 'temp',
-    imgSrc:
-      'https://149566639.v2.pressablecdn.com/wp-content/uploads/2021/11/aew-full-gear-1.jpg',
+    imgSrc: `${CLOUDINARY_LINK}/Media/PPV/FullGear2021.jpg`,
   },
 ];
 

61d41f1227247bd367499a54dbf20933a8fbe8e3
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-22 09:22:50 -0700

    Navbar: map links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,41 @@
 import { Search, Notifications, ArrowDropDown } from '@mui/icons-material';
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-
+  const links = [
+    {
+      linkTitle: 'Featured',
+      linkPath: '/',
+    },
+    {
+      linkTitle: 'Pay-Per View',
+      linkPath: '/payperview',
+    },
+    {
+      linkTitle: 'Dynamite',
+      linkPath: '/dynamite',
+    },
+    {
+      linkTitle: 'Rampage',
+      linkPath: '/rampage',
+    },
+    {
+      linkTitle: 'Dark',
+      linkPath: '/dark',
+    },
+    {
+      linkTitle: 'Dark Elevation',
+      linkPath: '/dark-elevation',
+    },
+  ];
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
 
-  console.log(isScrolled);
   return (
     <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
       <div className='container'>
@@ -19,12 +44,13 @@ const Navbar = () => {
             src='https://upload.wikimedia.org/wikipedia/en/6/6d/All_Elite_Wrestling.jpg'
             alt='All Elite Wrestling logo'
           />
-          <span>Featured</span>
-          <span>Pay-Per View</span>
-          <span>Dynamite</span>
-          <span>Rampage</span>
-          <span>Dark</span>
-          <span>Dark Elevation</span>
+          <div>
+            {links.map((link, i) => (
+              <span key={i}>
+                <NavLink to={link.linkPath}>{link.linkTitle}</NavLink>
+              </span>
+            ))}
+          </div>
         </div>
         <div className='right'>
           <Search />

2971a868dad060a96cfadaee446afc217068d94c
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-23 08:42:11 -0700

    Routes: added routes for all pages

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,8 +1,12 @@
 import Home from '../views/home/Home';
 import PPV from '../views/payperview/PPV';
 const ROUTES = [
-  { path: '/', exact: true, key: 'Home', component: Home },
-  { path: '/payperview', key: 'PPV', component: PPV },
+  { title: 'Featured', path: '/', exact: true, key: 'Home', component: Home },
+  { title: 'Pay-Per View', path: '/payperview', key: 'PPV', component: PPV },
+  { title: 'Dynamite', path: '/dynamite', key: 'Dynamite' },
+  { title: 'Rampage', path: '/rampage', key: 'Rampage' },
+  { title: 'Dark', path: '/dark', key: 'Dark' },
+  { title: 'Dark Elevation', path: '/darkelevation', key: 'Dark Elevation' },
 ];
 
 export default ROUTES;

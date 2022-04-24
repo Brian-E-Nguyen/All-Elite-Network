6396dfd2bd59fa3c297f54fa817c299599d2a07d
buraiyen <brian.edison.nguyen@gmail.com>
2022-04-24 12:03:14 -0700

    init Rampage.js; updated routes

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,6 +1,7 @@
 import Home from '../views/Home';
 import PPV from '../views/PPV';
 import Dynamite from '../views/Dynamite';
+import Rampage from '../views/Rampage';
 const ROUTES = [
   { title: 'Featured', path: '/', exact: true, key: 'Home', component: Home },
   { title: 'Pay-Per View', path: '/payperview', key: 'PPV', component: PPV },
@@ -10,7 +11,7 @@ const ROUTES = [
     key: 'Dynamite',
     component: Dynamite,
   },
-  { title: 'Rampage', path: '/rampage', key: 'Rampage' },
+  { title: 'Rampage', path: '/rampage', key: 'Rampage', component: Rampage },
   { title: 'Dark', path: '/dark', key: 'Dark' },
   { title: 'Dark Elevation', path: '/darkelevation', key: 'Dark Elevation' },
 ];

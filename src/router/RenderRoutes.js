3a9062f17e8b80a30d77b4de2b18725743443039
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-05-02 11:49:04 -0700

    RenderRoutes: implement animation

--- src/router/RenderRoutes.js
+++ src/router/RenderRoutes.js
@@ -1,20 +1,22 @@
 import { Route, Switch } from 'react-router-dom';
 import ROUTES from './Routes';
 import { useLocation } from 'react-router-dom';
-
+import { AnimatePresence } from 'framer-motion';
 const RenderRoutes = () => {
   const location = useLocation();
   return (
-    <Switch location={location} key={location.pathname}>
-      {ROUTES.map((route) => (
-        <Route
-          path={route.path}
-          key={route.key}
-          exact={route.exact}
-          component={route.component}
-        />
-      ))}
-    </Switch>
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.pathname}>
+        {ROUTES.map((route) => (
+          <Route
+            path={route.path}
+            key={route.key}
+            exact={route.exact}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+    </AnimatePresence>
   );
 };
 

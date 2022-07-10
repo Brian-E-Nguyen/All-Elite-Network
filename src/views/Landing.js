c0bdb51b5d5c1cacfbdc97200ba450dfa4c13b3c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-10 13:17:09 -0700

    Landing: import LandingJumbotron

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LandingJumbotron from '../components/landing/LandingJumbotron';
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <LandingJumbotron />
+    </div>
+  );
 };
 
 export default Landing;

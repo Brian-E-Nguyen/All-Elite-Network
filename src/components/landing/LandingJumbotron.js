4fac1a1e1b99ff85252bf02de2f62865ec9fc02c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-07-10 12:32:32 -0700

    LandingJumbotron: image

---
+++ src/components/landing/LandingJumbotron.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const styles = {
+  width: '100%',
+  height: '80vh',
+};
+
+const LandingJumbotron = () => {
+  return (
+    <>
+      <img
+        src='https://res.cloudinary.com/buraiyen/image/upload/v1657481435/All_Elite_Network/aew-dynamite-grand-slam-crowd_r0e4i3.jpg'
+        style={styles}
+        alt='AEW Dynamite stage with fireworks'
+      />
+    </>
+  );
+};
+
+export default LandingJumbotron;

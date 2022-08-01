f646696f8c16ec4ba798926bb66fb892c43ac84a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-01 13:08:29 -0700

    LandingInfo: pricing table

--- src/components/landing/LandingInfo.js
+++ src/components/landing/LandingInfo.js
@@ -23,7 +23,49 @@ const LandingInfo = () => {
         The All Elite Network is the ultimate unofficial streaming service for
         fans of All Elite Wrestling to access the AEW library of amazing content
       </p>
-      <h2>What You Will Receive</h2>
+      <h2>Select Your Plan</h2>
+      <table>
+        <tr>
+          <td>Monthly Price</td>
+          <td>$4.99/month</td>
+          <td>$6.99/month</td>
+        </tr>
+        <tr>
+          <td>Stream live PPV events and Dark (Elevation) tapings</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Access Dynamite and Rampage tapings one week after they air</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Instant access to entire YouTube library</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Watch on your TV, PC, phone, or tablet</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Support up to 3 devices</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>No ads from any third-party source</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Download any media to your device</td>
+          <td></td>
+          <td>✓</td>
+        </tr>
+      </table>
     </div>
   );
 };

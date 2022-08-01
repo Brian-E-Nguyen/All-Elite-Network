68edac89f8f5eb56db93af3080929ca50ab455c8
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-01 12:47:18 -0700

    LandingInfo: starting description

--- src/components/landing/LandingInfo.js
+++ src/components/landing/LandingInfo.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 const LandingInfo = () => {
-  return <div>LandingInfo</div>;
+  return (
+    <div>
+      <h2>Who we are</h2>
+      <p>
+        All Elite Wrestling (AEW) is an American wrestling company based in
+        Jacksonville, Florida. Since its inception in 2019, AEW has rapidly
+        changed the landscape of professional wrestling. It has and is
+        continuing to provide a great alternative for new and old wrestling
+        fans, and is considered the second largest professional wrestling
+        company behind Word Wrestling Entertainment (WWE).
+      </p>
+      <h2>The All Elite Network</h2>
+      <p>
+        For years, fans have been wanting a streaming service that provides
+        convenient access to live shows and archived content; this includes
+        AEW's quarterly Pay-Per Views, weekly TV shows (Dynamite and Rampage),
+        and weekly YouTube shows (Dark and Dark Elevation)
+      </p>
+      <p>
+        The All Elite Network is the ultimate unofficial streaming service for
+        fans of All Elite Wrestling to access the AEW library of amazing content
+      </p>
+      <h2>What You Will Receive</h2>
+    </div>
+  );
 };
 
 export default LandingInfo;

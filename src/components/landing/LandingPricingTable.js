c158e77b734921c85d299636fea7f3b1a2a1a50e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-01 15:01:39 -0700

    LandingPricingTable: temp styles

--- src/components/landing/LandingPricingTable.js
+++ src/components/landing/LandingPricingTable.js
@@ -1,48 +1,51 @@
 import React from 'react';
+import './LandingPricingTable.scss';
 
 const LandingPricingTable = () => {
   return (
-    <table>
-      <tr>
-        <td>Monthly Price</td>
-        <td>$4.99/month</td>
-        <td>$6.99/month</td>
-      </tr>
-      <tr>
-        <td>Stream live PPV events and Dark (Elevation) tapings</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>Access Dynamite and Rampage tapings one week after they air</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>Instant access to entire YouTube library</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>Watch on your TV, PC, phone, or tablet</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>Support up to 3 devices</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>No ads from any third-party source</td>
-        <td>✓</td>
-        <td>✓</td>
-      </tr>
-      <tr>
-        <td>Download any media to your device</td>
-        <td></td>
-        <td>✓</td>
-      </tr>
+    <table className='pricing_table'>
+      <tbody>
+        <tr>
+          <td>Monthly Price</td>
+          <td>$4.99/month</td>
+          <td>$6.99/month</td>
+        </tr>
+        <tr>
+          <td>Stream live PPV events and Dark (Elevation) tapings</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Access Dynamite and Rampage tapings one week after they air</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Instant access to entire YouTube library</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Watch on your TV, PC, phone, or tablet</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Support up to 3 devices</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>No ads from any third-party source</td>
+          <td>✓</td>
+          <td>✓</td>
+        </tr>
+        <tr>
+          <td>Download any media to your device</td>
+          <td></td>
+          <td>✓</td>
+        </tr>
+      </tbody>
     </table>
   );
 };

3cd3403d1c2bf12e6d53568d10f3aeac1f370893
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-01 13:17:20 -0700

    init LandingPricingTable

--- src/components/landing/LandingInfo.js
+++ src/components/landing/LandingInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import LandingPricingTable from './LandingPricingTable';
 const LandingInfo = () => {
   return (
     <div>
@@ -24,48 +24,7 @@ const LandingInfo = () => {
         fans of All Elite Wrestling to access the AEW library of amazing content
       </p>
       <h2>Select Your Plan</h2>
-      <table>
-        <tr>
-          <td>Monthly Price</td>
-          <td>$4.99/month</td>
-          <td>$6.99/month</td>
-        </tr>
-        <tr>
-          <td>Stream live PPV events and Dark (Elevation) tapings</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>Access Dynamite and Rampage tapings one week after they air</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>Instant access to entire YouTube library</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>Watch on your TV, PC, phone, or tablet</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>Support up to 3 devices</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>No ads from any third-party source</td>
-          <td>✓</td>
-          <td>✓</td>
-        </tr>
-        <tr>
-          <td>Download any media to your device</td>
-          <td></td>
-          <td>✓</td>
-        </tr>
-      </table>
+      <LandingPricingTable />
     </div>
   );
 };
---
+++ src/components/landing/LandingPricingTable.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const LandingPricingTable = () => {
+  return (
+    <table>
+      <tr>
+        <td>Monthly Price</td>
+        <td>$4.99/month</td>
+        <td>$6.99/month</td>
+      </tr>
+      <tr>
+        <td>Stream live PPV events and Dark (Elevation) tapings</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>Access Dynamite and Rampage tapings one week after they air</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>Instant access to entire YouTube library</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>Watch on your TV, PC, phone, or tablet</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>Support up to 3 devices</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>No ads from any third-party source</td>
+        <td>✓</td>
+        <td>✓</td>
+      </tr>
+      <tr>
+        <td>Download any media to your device</td>
+        <td></td>
+        <td>✓</td>
+      </tr>
+    </table>
+  );
+};
+
+export default LandingPricingTable;

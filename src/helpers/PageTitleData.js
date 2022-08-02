b9d2466fa458b9d6c94db0db3069f1c513bb2e2d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-08-01 17:17:30 -0700

    PageTitleData: import data

---
+++ src/helpers/PageTitleData.js
@@ -0,0 +1,22 @@
+const suffix = '| All Elite Network';
+
+const HomePageTitle = `All Elite Network`;
+
+const PPVPageTitle = `Pay Per View ${suffix}`;
+
+const DynamitePageTitle = `Dynamite ${suffix}`;
+
+const RampagePageTitle = `Rampage ${suffix}`;
+
+const DarkPageTitle = `Dark ${suffix}`;
+
+const DarkElevationPageTitle = `Dark Elevation ${suffix}`;
+
+export {
+  HomePageTitle,
+  PPVPageTitle,
+  DynamitePageTitle,
+  RampagePageTitle,
+  DarkPageTitle,
+  DarkElevationPageTitle,
+};

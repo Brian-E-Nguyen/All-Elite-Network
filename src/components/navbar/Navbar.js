157c271d3608b44fa9bb9a8fafbb9e71c4c44572
buraiyen <brian.edison.nguyen@gmail.com>
2022-08-03 15:21:13 -0700

    Navbar: mobile link layout

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -31,15 +31,19 @@ const Navbar = () => {
     }
   };
   return (
-    <nav className=" bg-red-500 text-white w-full top-0 fixed z-10">
-      <div className="flex items-center justify-between h-16 flex-row py-3">
+    <nav className="container-fluid bg-red-500 text-white w-full top-0  fixed z-10">
+      <div className=" flex flex-nowrap items-center justify-between h-16  w-full flex-row py-3 px-4">
         {/* Left */}
-        <div className="flex items-center">
+        <div className="inline-flex pb-5 absolute top-0 translate-x-5 translate-y-5 flex-col text-left">
           {ROUTES.map((route) => (
             <span key={route.key}>
               <NavLink
                 exact
-                className="transition-all duration-300 cursor-pointer uppercase mx-3 font-bold hover:text-eliteGold xs:invisible lg:visible"
+                className="transition-all duration-300 
+                          cursor-pointer 
+                          uppercase mx-3 font-bold 
+                          hover:text-eliteGold
+                          xs:text-3xl "
                 activeClassName={'text-eliteGold'}
                 to={route.path}
                 onClick={buttonClickHandler}
@@ -50,7 +54,7 @@ const Navbar = () => {
           ))}
         </div>
         {/* Right */}
-        <div className="flex items-center">
+        <div className="flex items-center border-solid border-4 border-red-100">
           <Search className="xs:invisible lg:visible" />
           <Notifications className="xs:invisible lg:visible" />
           <div
@@ -69,7 +73,7 @@ const Navbar = () => {
               <span>Logout</span>
             </div>
           </div>
-          <Menu className=" xs:visible lg:invisible" />
+          <Menu className="flex xs:visible lg:invisible" />
         </div>
       </div>
     </nav>

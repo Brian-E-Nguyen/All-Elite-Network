29d31f58b1839ee5d3feb0ddab607092f2b0b415
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-27 13:21:50 -0800

    init HomeView

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,11 @@
+const HomeView = () => {
+  return (
+    <div>
+      <h1 className='text-center'>
+        The Wrestling Streaming Platform You've Been Waiting For
+      </h1>
+    </div>
+  );
+};
+
+export default HomeView;

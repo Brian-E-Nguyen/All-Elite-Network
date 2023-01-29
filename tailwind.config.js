99fdd59aa02956503a2cbde9d1d4bc4612f2233f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 15:54:41 -0800

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      xs: '0px',
+      xs: { max: '639px' },
       sm: '640px',
       md: '768px',
       lg: '1024px',

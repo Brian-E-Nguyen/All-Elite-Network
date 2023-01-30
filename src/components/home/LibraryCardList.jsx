236c59291e31a69f16c352eb2833b6f3a4edd310
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 17:29:09 -0800

    fix: LibraryCardList truncated class names

--- src/components/home/LibraryCardList.jsx
+++ src/components/home/LibraryCardList.jsx
@@ -1,6 +1,18 @@
-export default function ShowCards() {
+export default function LibraryCardList() {
+  const gridContainer = [
+    'grid',
+    'grid-cols-4',
+    'gap-4',
+    'xs:grid-cols-1',
+    'sm:grid-cols-2',
+    'lg:grid-cols-4',
+    'items-center',
+    'justify-center',
+    'text-center',
+  ].join(' ');
+
   return (
-    <section className='grid grid-cols-4 gap-4 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center text-center'>
+    <section className={gridContainer}>
       <div className='col-span-1'>Column 1</div>
       <div className='col-span-1'>Column 2</div>
       <div className='col-span-1'>Column 3</div>

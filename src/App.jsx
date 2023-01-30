b20e360033d1eb8e31fe7f6025b631c95fc18cd9
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-30 12:44:17 -0800

    init navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from './components/ui/navbar/Navbar';
 import HomeView from './views/HomeView';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <HomeView />
     </div>
   );

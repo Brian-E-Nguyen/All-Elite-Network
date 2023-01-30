ce2a8541d3d71d8e0418b8d6e59dcae88cbddf13
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 19:06:46 -0800

    init FeaturedData

---
+++ src/data/FeaturedData.jsx
@@ -0,0 +1,6 @@
+const cloudinaryName = proces.env.CLOUDINARY_NAME;
+
+export const homeFeaturedData = {
+  src: `https://res.cloudinary.com/${cloudinaryName}/image/upload/v1657480906/All_Elite_Network/aew-stage-fireworks.png`,
+  alt: 'AEW stage with fireworks erupting',
+};

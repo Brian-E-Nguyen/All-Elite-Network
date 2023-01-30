db4bb15b9b2e2145d03c78a48db7e260224038e5
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 17:58:29 -0800

    import LibraryCard to LibraryCardList

--- src/components/home/LibraryCardList.jsx
+++ src/components/home/LibraryCardList.jsx
@@ -1,10 +1,10 @@
+import LibraryCard from './LibraryCard';
+
 export default function LibraryCardList() {
   const gridContainer = [
     'grid',
-    'grid-cols-4',
+    'grid-cols-2',
     'gap-4',
-    'xs:grid-cols-1',
-    'sm:grid-cols-2',
     'lg:grid-cols-4',
     'items-center',
     'justify-center',
@@ -13,10 +13,10 @@ export default function LibraryCardList() {
 
   return (
     <section className={gridContainer}>
-      <div className='col-span-1'>Column 1</div>
-      <div className='col-span-1'>Column 2</div>
-      <div className='col-span-1'>Column 3</div>
-      <div className='col-span-1'>Column 4</div>
+      <LibraryCard />
+      <LibraryCard />
+      <LibraryCard />
+      <LibraryCard />
     </section>
   );
 }

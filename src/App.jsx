7fd9144e071920e8c838f9697f308b80e7a3a614
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 20:00:49 -0800

    .gitignore and HomeView import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import HomeView from './views/HomeView';
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='bg-yellow-600'>Test</h1>
+      <HomeView />
     </div>
   );
 }

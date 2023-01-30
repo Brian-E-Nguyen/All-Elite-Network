f43840e22973ac5f4130407e5f5bc87ded864f02
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 19:54:15 -0800

    full LibraryCard data

--- src/data/LibraryCardData.jsx
+++ src/data/LibraryCardData.jsx
@@ -1,18 +1,22 @@
 export default [
-  { title: 'PPV', src: '', alt: '' },
+  {
+    title: 'PPV',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1675049986/All_Elite_Network/Media/PPV/revolution_myqtyc.webp',
+    alt: 'AEW Revolution 2022 poster featuring many wrestlers',
+  },
   {
     title: 'Dynamite',
-    src: '',
-    alt: '',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1675050168/All_Elite_Network/Media/Dynamite/Dynamite.webp',
+    alt: 'AEW Dynamite poster featuring many wrestlers',
   },
   {
     title: 'Rampage',
-    src: '',
-    alt: '',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1675050168/All_Elite_Network/Media/Rampage/rampage.webp',
+    alt: 'AEW Rampage poster featuring many wrestlers',
   },
   {
     title: 'Dark / Dark Elevation',
-    src: '',
-    alt: '',
+    src: 'https://res.cloudinary.com/buraiyen/image/upload/v1675050719/All_Elite_Network/Media/Dark/dark.webp',
+    alt: 'AEW Dark / Dark Elevation poster featuring many wrestlers',
   },
 ];

dafd4ff24f3678f757b1a459009ac2d1f1f73816
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 17:26:09 -0800

    fix: renamed ShowCards to LibraryCardList

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,6 +1,6 @@
 import Plans from '../components/home/Plans';
 import Info from '../components/home/Info';
-import ShowCards from '../components/home/ShowCards';
+import LibraryCardList from '../components/home/LibraryCardList';
 import HomeHeader from '../components/home/HomeHeader';
 
 export default function HomeView() {
@@ -8,7 +8,7 @@ export default function HomeView() {
     <>
       <HomeHeader />
       <div className='container sm:w-3/4 md:w-2/3 lg:w-screen mx-auto'>
-        <ShowCards />
+        <LibraryCardList />
         <Info />
         <Plans />
       </div>

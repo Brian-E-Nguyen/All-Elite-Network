8aa9b959e42a76976c51cced6de52105999aa0c8
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-29 17:21:52 -0800

    feat: temporary background

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 export default function HomeHeader() {
   return (
-    <section className='text-center'>
-      <h1>All Elite Wrestling: Now Available on Demand</h1>
-      <p>
-        Get access to the extensive All Elite Wrestling digital library,
-        including PPV's, Dynamite, Rampage, Dark (Elevation), and more
-      </p>
+    <section className='relative h-96 text-center bg-slate-500'>
+      <div className='absolute inset-0 flex items-center justify-center'>
+        <div>
+          <h1>All Elite Wrestling: Now Available on Demand</h1>
+          <p>
+            Get access to the extensive All Elite Wrestling digital library,
+            including PPV's, Dynamite, Rampage, Dark (Elevation), and more
+          </p>
+        </div>
+      </div>
     </section>
   );
 }

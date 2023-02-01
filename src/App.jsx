ef98df48485ea198dbfd8a6768859f36fe0f4d2c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 13:48:15 -0800

    configured router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Footer from './components/ui/footer/Footer';
 import Navbar from './components/ui/navbar/Navbar';
+import RenderRoutes from './routes/RenderRoutes';
 import HomeView from './views/HomeView';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <HomeView />
+      <RenderRoutes />
       <Footer />
     </div>
   );
--- src/routes/RenderRoutes.jsx
+++ src/routes/RenderRoutes.jsx
@@ -1,12 +1,11 @@
-import { Route, Switch } from 'react-router-dom';
-import ROUTES from './Routes';
-import { useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import Routes from './Routes';
 
 export default function RenderRoutes() {
   const location = useLocation();
   return (
-    <Switch location={location} key={location.pathname}>
-      {ROUTES.map((route) => (
+    <Switch key={location.pathname}>
+      {Routes.map((route) => (
         <Route
           path={route.path}
           key={route.key}

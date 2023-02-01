d9a1292474c6b2d24244e23828dc08a46bb49cc3
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 14:06:37 -0800

    merge mistake fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Footer from './components/ui/footer/Footer';
-import Navbar from './components/ui/navbar/Navbar';
 import RenderRoutes from './routes/RenderRoutes';
-import HomeView from './views/HomeView';
 
 function App() {
   return (
     <div className='App'>
-      <HomeView />
+      <RenderRoutes />
       <Footer />
     </div>
   );

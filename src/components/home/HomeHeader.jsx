a0839478f93e48fe892812b247397bb3aa7e5a18
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 12:39:50 -0800

    HomeHeader: login link

--- src/components/home/HomeHeader.jsx
+++ src/components/home/HomeHeader.jsx
@@ -1,7 +1,6 @@
 import { homeFeaturedData } from '../../data/FeaturedData';
 
 export default function HomeHeader() {
-  console.log(homeFeaturedData);
   return (
     <section className='relative text-center' style={{ height: '80vh' }}>
       <img
@@ -12,12 +11,15 @@ export default function HomeHeader() {
       <div className='absolute inset-0 flex items-center justify-center'>
         <div>
           <h1>All Elite Wrestling: Now Available on Demand</h1>
-          <p className='text-lg'>
+          <p className='text-lg font-bold'>
             Welcome to the All-Elite Network. Get access to the extensive All
             Elite Wrestling digital library, including PPV's, Dynamite, Rampage,
             Dark (Elevation), and more
           </p>
-          <button className='w-1/2 py-5'>Sign Up Now</button>
+          <button className='w-1/2 py-5 mb-11'>Sign Up Now</button>
+          <p className='font-bold'>
+            Already have an account? <a href='#'>Login here!</a>
+          </p>
         </div>
       </div>
     </section>

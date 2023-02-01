0f9b7ad78ae55bd69c7d185473cf16b7eabbf79e
buraiyen <brian.edison.nguyen@gmail.com>
2023-01-31 16:37:57 -0800

    Info: container

--- src/components/home/Info.jsx
+++ src/components/home/Info.jsx
@@ -1,30 +1,36 @@
 export default function Info() {
   return (
-    <section className='grid gap-4 lg:grid-cols-2 sm:grid-cols-1 justify-center mb-32'>
-      <div>
-        <h2>The Wrestling Streaming Service You've Been Waiting For</h2>
-        <div className='text-xl'>
-          <p>
-            For years, fans have been wanting a streaming service that provides
-            convenient access to live shows and archived content; this includes
-            AEW's quarterly Pay-Per Views, weekly TV shows (Dynamite and
-            Rampage), and weekly YouTube shows (Dark and Dark Elevation).
-          </p>
-          <p>
-            The All Elite Network is the ultimate{' '}
-            <em>
-              <u>unofficial</u>
-            </em>{' '}
-            streaming service for fans of All Elite Wrestling to access the AEW
-            library of amazing content.
-          </p>
+    <section className='bg-neutral-900 py-4'>
+      <div className='container sm:w-3/4 md:w-2/3 lg:w-screen mx-auto'>
+        <div className='grid gap-4 lg:grid-cols-2 sm:grid-cols-1 justify-center mb-32'>
+          <div>
+            <h2>The Wrestling Streaming Service You've Been Waiting For</h2>
+            <div className='text-xl'>
+              <p>
+                For years, fans have been wanting a streaming service that
+                provides convenient access to live shows and archived content;
+                this includes AEW's quarterly Pay-Per Views, weekly TV shows
+                (Dynamite and Rampage), and weekly YouTube shows (Dark and Dark
+                Elevation).
+              </p>
+              <p>
+                The All Elite Network is the ultimate{' '}
+                <em>
+                  <u>unofficial</u>
+                </em>{' '}
+                streaming service for fans of All Elite Wrestling to access the
+                AEW library of amazing content.
+              </p>
+            </div>
+          </div>
+          <div>
+            <img
+              src='https://res.cloudinary.com/buraiyen/image/upload/v1675106463/All_Elite_Network/adam-page-dynamite-october-6-1200x675_zltdbt.webp'
+              alt='Wrestler "Hangman" Adam Page standing on a ladder, eyeing the crowd after winning a ladder match'
+              loading='lazy'
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <img
-          src='https://res.cloudinary.com/buraiyen/image/upload/v1675106463/All_Elite_Network/adam-page-dynamite-october-6-1200x675_zltdbt.webp'
-          alt='Wrestler "Hangman" Adam Page standing on a ladder, eyeing the crowd after winning a ladder match'
-        />
       </div>
     </section>
   );

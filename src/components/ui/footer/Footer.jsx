4d8822cd1420bf1ed1333ea77007214accc33633
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-01 12:30:49 -0800

    init Footer

--- src/components/ui/footer/Footer.jsx
+++ src/components/ui/footer/Footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className='bg-black py-11 text-neutral-500'>
+      <div className='container w-3/4 mx-auto'>
+        <div className='flex justify-center'>
+          <img
+            src='https://res.cloudinary.com/buraiyen/image/upload/v1675282964/All_Elite_Network/aew-logo_fpfxqw.png'
+            alt='AEW Logo'
+            className='w-60'
+          />
+        </div>
+        <div>Gigatummy</div>
+      </div>
     </footer>
   );
 }

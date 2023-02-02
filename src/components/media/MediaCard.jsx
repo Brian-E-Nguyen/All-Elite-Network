a8f86a6b6764ebca905d292860c642054f43bab4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 13:14:31 -0800

    feat: MediaCard click event

--- src/components/media/MediaCard.jsx
+++ src/components/media/MediaCard.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function MediaCard() {
+  const [isOpen, setIsOpen] = useState(false);
   const temp =
-    'https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fshowmax.akamaized.net%2Fi%2F12cdc789-bff1-47db-9f18-622c8565fa33%2Fformat%3Dwebp%2Fq%3D70%2F850x&sp=1675289558T8a3bc6a6c151a3e46bf20dbd1831a85f6485536ace5afa39444d88a542c430ae';
+    'https://m.media-amazon.com/images/M/MV5BNDUwNjBkMmUtZjM2My00NmM4LTlmOWQtNWE5YTdmN2Y2MTgxXkEyXkFqcGdeQXRyYW5zY29kZS13b3JrZmxvdw@@._V1_.jpg';
+
+  function clickHandler() {
+    setIsOpen((currentState) => !currentState);
+  }
   return (
-    <div>
-      <img src={temp} alt='' />
+    <div
+      onClick={clickHandler}
+      className={
+        'w-[250px] h-[120px] bg-black px-4 overflow-hidden transition duration-300 ease-in-out ' +
+        (isOpen ? 'h-64' : '')
+      }
+    >
+      <img src={temp} alt='' className='w-full h-[120px] bg-cover' />
+      <div>
+        <h2>Title</h2>
+        <h3>Runtime</h3>
+        <h3>Data</h3>
+        <p>Description</p>
+      </div>
     </div>
   );
 }

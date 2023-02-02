db84bde2789f370edb9499f38e23d646c35ef87f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-02 13:40:38 -0800

    feat: MediaCardList grid styles

--- src/components/media/MediaCardList.jsx
+++ src/components/media/MediaCardList.jsx
@@ -1,11 +1,22 @@
 import MediaCard from './MediaCard';
 
 export default function MediaCardList({ shows }) {
+  const gridContainer = [
+    'grid',
+    'grid-cols-2',
+    'gap-4',
+    'lg:grid-cols-4',
+    'justify-center',
+    'mb-32',
+    'container',
+    'mx-auto',
+  ].join(' ');
+
   return (
-    <div>
+    <section className={gridContainer}>
       {shows.map((show, i) => (
         <MediaCard show={show} />
       ))}
-    </div>
+    </section>
   );
 }

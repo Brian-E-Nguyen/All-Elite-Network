bb1f98bded8829e619ddd08cd0b92fddc8b98052
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-03 13:05:25 -0800

    feat: import MediaCardList to FeaturedView

--- src/views/FeaturedView.jsx
+++ src/views/FeaturedView.jsx
@@ -1,5 +1,24 @@
+import Featured from '../components/ui/Featured';
+import MediaCardList from '../components/media/MediaCardList';
+import {
+  payperview,
+  dynamite,
+  rampage,
+  dark,
+  darkElevation,
+} from '../data/MediaListData';
+
 function FeaturedView() {
-  return <div>FeaturedView</div>;
+  return (
+    <>
+      <Featured />
+      <MediaCardList shows={payperview} />
+      <MediaCardList shows={dynamite} />
+      <MediaCardList shows={rampage} />
+      <MediaCardList shows={dark} />
+      <MediaCardList shows={darkElevation} />
+    </>
+  );
 }
 
 export default FeaturedView;

cd9dce2b66e12f6cff2e435f28af78f0cef2d7a0
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-03 12:33:33 -0800

    feat: route for FeaturedView

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,14 @@
 import HomeView from '../views/HomeView';
+import FeaturedView from '../views/FeaturedView';
 
 const Routes = [
   { title: 'Home', path: '/', exact: true, key: 'Home', component: HomeView },
+  {
+    title: 'Featured',
+    path: '/featured',
+    key: 'Featured',
+    component: FeaturedView,
+  },
 ];
 
 export default Routes;

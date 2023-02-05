17cda6f2a47080900496876cf1af6a7a3f6d016e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-05 14:10:34 -0800

    feat: DynamiteView content

--- src/views/DynamiteView.jsx
+++ src/views/DynamiteView.jsx
@@ -1,5 +1,27 @@
+import Jumbotron from '../components/ui/jumbotron/Jumbotron';
+import MediaCardList from '../components/media/MediaCardList';
+import Navbar from '../components/ui/navbar/Navbar';
+import { dynamite } from '../data/MediaListData';
+
 function DynamiteView() {
-  return <div>DyanmiteView</div>;
+  const jumboImg = dynamite[0].imgSrc;
+  const jumboDescription = dynamite[0].description;
+  return (
+    <>
+      <Navbar />
+      <Jumbotron src={jumboImg} description={jumboDescription} />
+      <section className='container mx-auto'>
+        <h1 className='text-center'>AEW Dynamite</h1>
+        <p className='text-xl'>
+          AEW's flagship weekly show that features the best wrestlers from
+          across the world. With its hard-hitting and highly-flighing
+          athleticism, unpredictable outcomes, and intense rivalries, Dynamite
+          is your go-to weekly program for all things wrestling.
+        </p>
+      </section>
+      <MediaCardList shows={dynamite} showName='2022' />
+    </>
+  );
 }
 
 export default DynamiteView;

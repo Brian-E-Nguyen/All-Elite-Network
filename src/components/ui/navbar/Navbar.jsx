0e211a2b39b2ba9915229f7d0540150fa3d14da4
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-05 18:54:20 -0800

    feat: navbar mobile button to separate component

--- src/components/ui/navbar/Navbar.jsx
+++ src/components/ui/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import NavbarMobileButton from './NavbarMobileButton';
+
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const navLinks = [
@@ -23,48 +25,7 @@ function Navbar() {
         </ul>
       </div>
       <div className='mr-12 absolute right-0 flex md:hidden'>
-        <button
-          onClick={() => setIsOpen((currentState) => !currentState)}
-          type='button'
-          className='bg-yellow-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
-          aria-controls='mobile-menu'
-          aria-expanded='false'
-        >
-          <span className='sr-only'>Open main menu</span>
-          {!isOpen ? (
-            <svg
-              className='block h-6 w-6'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h16M4 18h16'
-              />
-            </svg>
-          ) : (
-            <svg
-              className='block h-6 w-6'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M6 18L18 6M6 6l12 12'
-              />
-            </svg>
-          )}
-        </button>
+        <NavbarMobileButton isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <div className='md:hidden'>
         {isOpen && (

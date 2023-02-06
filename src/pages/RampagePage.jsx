477189faf20cd4344ca23cbc00300894525bda16
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-05 18:31:06 -0800

    feat: RampagePage content

--- src/pages/RampagePage.jsx
+++ src/pages/RampagePage.jsx
@@ -1,5 +1,29 @@
+import Jumbotron from '../components/ui/jumbotron/Jumbotron';
+import MediaCardList from '../components/media/MediaCardList';
+import Navbar from '../components/ui/navbar/Navbar';
+import { rampage } from '../data/MediaListData';
+
 function RampagePage() {
-  return <div>RampagePage</div>;
+  const jumboImg = rampage[0].imgSrc;
+  const jumboTitle = rampage[0].title;
+  const jumboDescription = rampage[0].description;
+  return (
+    <>
+      <Navbar />
+      <Jumbotron
+        src={jumboImg}
+        title={jumboTitle}
+        description={jumboDescription}
+      />
+      <section className='container mx-auto'>
+        <h1 className='text-center'>AEW Rampage</h1>
+        <p className='text-xl'>
+          Cap your week off with some exciting wrestling every Friday night.
+        </p>
+      </section>
+      <MediaCardList shows={rampage} showName='2022' />
+    </>
+  );
 }
 
 export default RampagePage;

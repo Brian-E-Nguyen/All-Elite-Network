29357180141dee1497ac0d934a02683c34e0db88
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-05 19:36:38 -0800

    feat: Login page background

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,12 @@
+import { homeFeaturedData } from '../data/FeaturedData';
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div
+      className='bg-cover bg-center h-screen brightness-50'
+      style={{ backgroundImage: `url(${homeFeaturedData.src})` }}
+    ></div>
+  );
 }
 
 export default LoginPage;

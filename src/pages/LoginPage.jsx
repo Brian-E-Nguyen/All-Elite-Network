f2a6ab0e6b09c12fd1df47f760f768721f130262
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-05 19:47:37 -0800

    feat: fixed brightness, added card component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,17 @@ import { homeFeaturedData } from '../data/FeaturedData';
 
 function LoginPage() {
   return (
-    <div
-      className='bg-cover bg-center h-screen brightness-50'
-      style={{ backgroundImage: `url(${homeFeaturedData.src})` }}
-    ></div>
+    <>
+      <div
+        className='relative bg-cover bg-center h-screen brightness-50'
+        style={{ backgroundImage: `url(${homeFeaturedData.src})` }}
+      ></div>
+      <section className='absolute left-0 top-10 right-0 bottom-0 mx-auto bg-neutral-700 w-[33%] h-[600px] rounded-xl'>
+        <div className='container mx-auto mt-5 text-center'>
+          <h1>Login</h1>
+        </div>
+      </section>
+    </>
   );
 }
 

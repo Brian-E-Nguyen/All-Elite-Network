b06600ffaba7de67e8f58105c5a1769cf18a63e9
buraiyen <brian.edison.nguyen@gmail.com>
2023-02-07 15:46:59 -0800

    init SignupPage

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,11 @@
+import { homeFeaturedData } from '../data/FeaturedData';
+export default function SignupPage() {
+  return (
+    <>
+      <div
+        className='relative bg-cover bg-center h-screen brightness-50'
+        style={{ backgroundImage: `url(${homeFeaturedData.src})` }}
+      ></div>
+    </>
+  );
+}

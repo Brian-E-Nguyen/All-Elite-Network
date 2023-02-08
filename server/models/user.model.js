2fe5b17e604b45397f825143e64de9eec7bd4493
buraiyen <brian.edison.nguyen@gmail.com>
2023-02-07 21:18:45 -0800

    init user.model.js

---
+++ server/models/user.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { collection: 'user-data' }
+);

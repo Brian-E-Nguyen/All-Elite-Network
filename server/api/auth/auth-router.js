9e787f1323ea2f7d78ade3c0da59be0392fa1dff
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-08 12:37:07 -0800

    fix: return statements for signup api

--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -20,13 +20,13 @@ router.post(
       role: 'user',
     })
       .then((result) => {
-        res.status(200).json({
+        return res.status(200).json({
           message: 'User registration completed',
           user: result,
         });
       })
       .catch((err) => {
-        res.status(500).json({
+        return res.status(500).json({
           message: 'User registration failed',
           stack: err.stack,
         });

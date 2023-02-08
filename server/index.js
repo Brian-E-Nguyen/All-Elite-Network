ceb8ab7c0ab4e6ed1492b53288bcb3c87df3409a
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-08 10:55:46 -0800

    feat: mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 1337;
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/all-elite-network');
 
 app.post('/api/register', (req, res) => {
   res.status(200).json({ message: 'register' });

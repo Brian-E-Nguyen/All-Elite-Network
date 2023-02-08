2790131a61250ab6c4a810e9768648048f1dfb6f
buraiyen <brian.edison.nguyen@gmail.com>
2023-02-07 20:46:00 -0800

    installed cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = 1337;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server started on localhost:${PORT}`);

c33f5c91243e743e6b1898b506d5dd40648b4fc8
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-08 12:24:19 -0800

    feat: checkEmailExists middleware

--- server/api/auth/auth-middleware.js
+++ server/api/auth/auth-middleware.js
@@ -10,4 +10,23 @@ function validatePayload(req, res, next) {
   next();
 }
 
-module.exports = { validatePayload };
+async function checkEmailExists(req, res, next) {
+  const { email } = req.body;
+  await User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          message: 'Email already exists',
+        });
+      }
+      next();
+    })
+    .catch((err) => {
+      res.staus(500).json({
+        message: 'Error finding email',
+        stack: err.stack,
+      });
+    });
+}
+
+module.exports = { validatePayload, checkEmailExists };
--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -1,29 +1,37 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/user.model');
-const { validatePayload } = require('./auth-middleware');
+const { validatePayload, checkEmailExists } = require('./auth-middleware');
 
-router.post('/register', validatePayload, async (req, res) => {
-  const user = req.body;
-  const hash = bcrypt.hashSync(user.password, process.env.VITE_BCRYPT_ROUNDS);
-  user.password = hash;
-  await User.create({
-    email: user.email,
-    password: user.password,
-    role: 'user',
-  })
-    .then((result) => {
-      res.status(200).json({
-        message: 'User registration completed',
-        user: result,
-      });
+router.post(
+  '/register',
+  validatePayload,
+  checkEmailExists,
+  async (req, res) => {
+    // Password hash
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, process.env.VITE_BCRYPT_ROUNDS);
+    user.password = hash;
+
+    // Attempt user creation
+    await User.create({
+      email: user.email,
+      password: user.password,
+      role: 'user',
     })
-    .catch((err) => {
-      res.status(500).json({
-        message: 'User registration failed',
-        stack: err.stack,
+      .then((result) => {
+        res.status(200).json({
+          message: 'User registration completed',
+          user: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: 'User registration failed',
+          stack: err.stack,
+        });
       });
-    });
-});
+  }
+);
 
 module.exports = router;

8c86dd8952986667bcdd904ebf754f616ba41cf2
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-08 10:30:49 -0800

    feat: user model unique email and role

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema(
   {
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, required: true },
   },
   { collection: 'user-data' }
 );

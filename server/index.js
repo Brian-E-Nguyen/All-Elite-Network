b212042cdb97031312db822c975876c09b8e6f67
buraiyen <brian.edison.nguyen@gmail.com>
2023-02-07 20:49:55 -0800

    server test route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const app = express();
 const PORT = 1337;
 
 app.use(cors());
+app.use(express.json());
+
+app.post('/api/register', (req, res) => {
+  res.status(200).json({ message: 'register' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on localhost:${PORT}`);

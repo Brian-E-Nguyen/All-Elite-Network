ad92d1a6b208e437f2b19e7050427b2464a63042
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-08 11:20:40 -0800

    feat: user registration api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/user.model');
 
 const app = express();
 const PORT = 1337;
@@ -10,8 +11,26 @@ app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/all-elite-network');
 
-app.post('/api/register', (req, res) => {
-  res.status(200).json({ message: 'register' });
+app.post('/api/register', async (req, res) => {
+  console.log('register called');
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+      role: 'user',
+    });
+    if (user) {
+      res.status(200).json({
+        message: 'User registration completed',
+        user,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'User registration failed',
+      stack: err.stack,
+    });
+  }
 });
 
 app.listen(PORT, () => {

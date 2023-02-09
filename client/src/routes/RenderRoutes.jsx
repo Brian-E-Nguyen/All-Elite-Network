b60aeb5ba872afc733cc301f1a1ffed99d606f44
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-02-09 12:21:20 -0800

    feat: animations for all routes

--- client/src/routes/RenderRoutes.jsx
+++ client/src/routes/RenderRoutes.jsx
@@ -1,18 +1,21 @@
 import { Route, Switch, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Routes from './Routes';
 
 export default function RenderRoutes() {
   const location = useLocation();
   return (
-    <Switch key={location.pathname}>
-      {Routes.map((route) => (
-        <Route
-          path={route.path}
-          key={route.key}
-          exact={route.exact}
-          component={route.component}
-        />
-      ))}
-    </Switch>
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.pathname}>
+        {Routes.map((route) => (
+          <Route
+            path={route.path}
+            key={route.key}
+            exact={route.exact}
+            component={route.component}
+          />
+        ))}
+      </Switch>
+    </AnimatePresence>
   );
 }

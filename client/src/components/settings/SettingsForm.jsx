89224a893baad3b9f436686a30b0373ce306fe17
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-03-09 09:10:53 -0800

    init SettingsForm

---
+++ client/src/components/settings/SettingsForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SettingsForm() {
+  return <div>SettingsForm</div>;
+}
+
+export default SettingsForm;
--- client/src/views/SettingsPage.jsx
+++ client/src/views/SettingsPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import Navbar from '../components/ui/navbar/Navbar';
+import AnimatedPage from '../components/animations/AnimatedPage';
 
 function SettingsPage() {
-  return <div>SettingsPage</div>;
+  return (
+    <>
+      <Navbar />
+      <AnimatedPage>
+        <section>
+          <h1>Settings</h1>
+          <button className='bg-red-600'>Delete Account</button>
+        </section>
+      </AnimatedPage>
+    </>
+  );
 }
 
 export default SettingsPage;

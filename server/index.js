c913a75230babc82d6d7de12b4e752a445ad565c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-03-27 14:03:39 -0700

    fix: mongodb connection based on environment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const server = require('./api/server');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+// dotenv.config({ path: '../.env' });
 
-const mongoUrl = `mongodb+srv://Buraiyen:${process.env.VITE_APP_MONGO_PASSWORD}@all-elite-network.fagkgiv.mongodb.net/?retryWrites=true&w=majority`;
-
-mongoose.connect(mongoUrl);
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '../.env.production.local' });
+  const mongoUrl = `mongodb+srv://Buraiyen:${process.env.VITE_APP_MONGO_PASSWORD}@all-elite-network.fagkgiv.mongodb.net/?retryWrites=true&w=majority`;
+  mongoose.connect(mongoUrl);
+} else {
+  dotenv.config({ path: '../.env.development.local' });
+  const mongoUrl =
+    'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.2';
+  mongoose.connect(mongoUrl);
+}
 
 const PORT = process.env.PORT || 1337;
 

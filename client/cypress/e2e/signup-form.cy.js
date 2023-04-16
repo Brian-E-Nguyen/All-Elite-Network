fa584a5ce9d64a84d2e50b1aec8fb41959f1871b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-04-16 06:50:23 -0700

    test: invalid passwords

--- client/cypress/e2e/signup-form.cy.js
+++ client/cypress/e2e/signup-form.cy.js
@@ -41,4 +41,26 @@ describe('Signup form errors', () => {
     cy.get('[data-cy="loading-animation"]').should('not.exist');
     cy.get('[data-cy="form-error"]').contains(/email already exists/i);
   });
+
+  it('Throws an error for invalid password', () => {
+    cy.get('input[name="email"]').type('foo@bar.baz');
+    cy.get('input[name="password"]').type('1');
+    cy.get('input[name="retypedPassword"]').type('1');
+    cy.get('input[name="plan"][value="all-elite"]').click();
+    cy.get('[data-cy="submit"]').click();
+    cy.get('[data-cy="loading-animation"]').should('not.exist');
+    cy.get('[data-cy="form-error"]').contains(
+      /password must be at least 6 characters/i
+    );
+  });
+
+  it("Throws an error when passwords don't match", () => {
+    cy.get('input[name="email"]').type('foo@bar.baz');
+    cy.get('input[name="password"]').type('11111111111');
+    cy.get('input[name="retypedPassword"]').type('22222222222222');
+    cy.get('input[name="plan"][value="all-elite"]').click();
+    cy.get('[data-cy="submit"]').click();
+    cy.get('[data-cy="loading-animation"]').should('not.exist');
+    cy.get('[data-cy="form-error"]').contains(/passwords do not match/i);
+  });
 });
